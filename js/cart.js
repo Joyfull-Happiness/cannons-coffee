343447d8f4ee55f323f7bced1971f1fbc8bbec83
Joyfull-Happiness <lajoie.bradley2@gmail.com>
2025-04-17 13:10:57 -0700

    updated js

---
+++ js/cart.js
@@ -0,0 +1,27 @@
+/* JavaScript coming soon! */
+let containerCart = document.querySelector(".parentCart");
+
+let form = document.querySelector("form");
+form.addEventListener("submit", onFormSubmit);
+console.log("form:", form);
+
+function onFormSubmit() {
+  event.preventDefault();
+  const data = new FormData(event.target);
+  const dataObject = Object.fromEntries(data.entries());
+  console.log(dataObject);
+
+  form.reset();
+
+  let nameCart = dataObject.nameCart;
+  let emailCart = dataObject.emailCart;
+  let passwordCart = dataObject.passwordCart;
+
+  let messageCart = `Welcome ${name}! You are loged in!`;
+
+  console.log("first message to user:", messageCart);
+
+  let messageParagraph = document.createElement("p");
+  messageParagraph.textContent = messageCart;
+  containerCart.appendChild(messageParagraph);
+}
--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,30 @@
 /* JavaScript coming soon! */
+let container = document.querySelector(".parentfour");
+
+let form = document.querySelector("form");
+form.addEventListener("submit", onFormSubmit);
+console.log("form:", form);
+
+function onFormSubmit() {
+  event.preventDefault();
+  const data = new FormData(event.target);
+  const dataObject = Object.fromEntries(data.entries());
+  console.log(dataObject);
+
+  form.reset();
+
+  let name = dataObject.name;
+  let email = dataObject.email;
+  let phone = dataObject.phone;
+  let origin = dataObject.origin;
+  let radio = dataObject.radio;
+  let comment = dataObject.comment;
+
+  let message = `Thank you ${name} for submitting your email ${email} and phone number ${phone}. ${origin} is a great choice! We will get back to you about ${radio} shortly. `;
+
+  console.log("first message to user:", message);
+
+  let messageParagraph = document.createElement("p");
+  messageParagraph.textContent = message;
+  container.appendChild(messageParagraph);
+}
